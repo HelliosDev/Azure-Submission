a286e2e819d0d4b049f782c2dfd483d3cfbc4076
HelliosDev <wiky.pedia@yahoo.com>
2019-04-04 15:30:34 +0700

    Initial Commit

--- process_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require "config.php";
-    require "functions.php";
-    
-    $id = $_GET["id"];
-    $sql_delete = "DELETE FROM student WHERE id=$id";
-    $conn->exec($sql_delete);
-    show_alert("Data has been deleted!");
-?>
